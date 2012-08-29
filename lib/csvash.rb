42b7c143c74128482a97609ebab667d52141d48f
Lukas Alexandre <lukeskytm@gmail.com>
2012-08-29 01:18:50 -0300

    Adding te ability to create model instance from the csv

--- lib/csvash.rb
+++ lib/csvash.rb
@@ -3,6 +3,24 @@ require 'csv'
 
 module Csvash
   def self.import_from_path(path)
+    hashify path
+  end
+
+  def self.hashify(path)
+    import path do |collection, current_line|
+      collection << current_line
+    end
+  end
+
+  def self.modelify(path, klass)
+    klass = klass.to_s.classify.constantize if klass.is_a?(String) || klass.is_a?(Symbol)
+    params = import path do |collection, current_line|
+      collection << klass.new(current_line)
+    end
+  end
+
+private
+  def self.import(path, &block)
     cols = nil
     first_line = true
     collection = []
@@ -17,8 +35,8 @@ module Csvash
 
       # map the csv columns with the current line values
       current_line = Hash[cols.zip(line.flatten)]
-      # storing the final line result
-      collection << current_line
+
+      block.call(collection, current_line)
     end
     collection
   end
